cb6087e5a643a0a1294775180b689f59d981a2d8
KhanhTIMN <khanhlcbb@gmail.com>
2025-04-27 01:14:04 +0700

    Ported to 1.21.4

--- src/main/java/dev/neddslayer/sharedhealth/components/SharedExhaustionComponent.java
+++ src/main/java/dev/neddslayer/sharedhealth/components/SharedExhaustionComponent.java
@@ -27,7 +27,7 @@ public class SharedExhaustionComponent implements IExhaustionComponent {
 
 	@Override
 	public void readFromNbt(NbtCompound tag, RegistryWrapper.WrapperLookup registryLookup) {
-		this.exhaustion = tag.getFloat("Exhaustion").orElse(0.0f);  // Default to 0.0f if not present
+		this.exhaustion = tag.getFloat("Exhaustion");  // Default to 0.0f if not present
 
 	}
 
